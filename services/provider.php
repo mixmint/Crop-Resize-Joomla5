cf177842d59b3855a6f1c671f428bff798037b90
Mirosław Majka <mix@proask.pl>
2025-01-02 17:03:43 +0100

    Initial commit

---
+++ services/provider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @version 1.1
+ * @package System.cropresize plugin
+ * @author Mirosław Majka (mix@proask.pl)
+ * @copyright (C) 2024 Mirosław Majka <mix@proask.pl>
+ * @license GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ **/
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+use Joomla\Plugin\System\CropResize\Extension\CropResize;
+
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function register(Container $container): void
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $plugin = new CropResize(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('system', 'cropresize')
+                );
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
