bbb4bc5a75b7a0671bd5572fcf08ede65f15f3bc
Mirosław Majka <mix@proask.pl>
2025-03-07 19:40:41 +0100

    Change translation
    Change transtaltion, update version number

--- src/Extension/CropResize.php
+++ src/Extension/CropResize.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.2.1
+ * @version 1.2.2
  * @package System.cropresize plugin
  * @author Mirosław Majka (mix@proask.pl)
  * @copyright (C) 2024 Mirosław Majka <mix@proask.pl>
